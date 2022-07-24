e0aef9aa58fb93f6e8d6c389d5ec221e122dc9dd
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-24 10:12:09 +0530

    Create 1298.swift

---
+++ 1101-1200/1298.swift
@@ -0,0 +1,29 @@
+  func maxCandies(_ status: [Int], _ candies: [Int], _ keys: [[Int]], _ containedBoxes: [[Int]], _ initialBoxes: [Int]) -> Int {
+        var totalCandies = 0
+        var visited = Set<Int>()
+        var queue = initialBoxes
+        var openableBoxes = Set<Int>()
+
+        for box in 0..<status.count where  status[box] == 1 { openableBoxes.insert(box) }
+        
+        while !queue.isEmpty && !openableBoxes.isEmpty{
+            var nextLevel = [Int]()
+            
+            for box in queue {
+                if !visited.contains(box) {
+                    if status[box] == 1 || openableBoxes.contains(box) {
+                        totalCandies += candies[box]
+                        visited.insert(box)
+                        for key in keys[box] { openableBoxes.insert(key) }
+                        openableBoxes.remove(box)
+                        nextLevel.append(contentsOf: containedBoxes[box])
+                    } 
+                    else { nextLevel.append(box) }
+                }
+            }
+
+            queue = nextLevel
+        }
+
+        return totalCandies
+    }
