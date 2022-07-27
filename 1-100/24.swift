e57c7590daba215937255b23e4676262176cfe42
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-27 11:19:42 +0530

    Create 24.swift

---
+++ 1-100/24.swift
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+  
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        if head == nil || head?.next == nil { return head }
+        let res = head?.next
+        head?.next = swapPairs(res?.next)
+        res?.next = head
+        return res
+    }
+    
+}
