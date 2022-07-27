593c46f1b15b0eed51ce8834220b4561d9fbf118
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-27 11:32:33 +0530

    Create 28.swift

---
+++ 1-100/28.swift
@@ -0,0 +1,15 @@
+class Solution {
+      func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        guard !nums.isEmpty else { return 0 }
+        var i = 0
+
+        for num in nums {
+            guard num != val else { continue }
+            nums[i] = num
+            i += 1
+        }
+
+        return i
+    }
+
+}
