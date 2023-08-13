991010dca2303d0383042f98105a5b44a9fdb31c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 14:13:35 +0530

    Create 2607.swift

---
+++ 2501-3000/2607.swift
@@ -0,0 +1,22 @@
+class Solution {
+
+
+    func makeSubKSumEqual(_ arr: [Int], _ k: Int) -> Int {
+        var arrCopy = arr  // Create a mutable copy of the array
+        var res: Int = 0
+        for i in 0..<k {
+            var cycle = [Int]()
+            var j = i
+            while arrCopy[j] != 0 {
+                cycle.append(arrCopy[j])
+                arrCopy[j] = 0
+                j = (j + k) % arrCopy.count
+            }
+            cycle.sort()
+            for n in cycle {
+                res += abs(n - cycle[cycle.count / 2])
+            }
+        }
+        return res
+    }
+}
