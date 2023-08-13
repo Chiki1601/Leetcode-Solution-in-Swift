7e8cf49b7ee10546dce116aa812747fba6165686
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 13:20:24 +0530

    Create 2518.swift

---
+++ 2501-3000/2518.swift
@@ -0,0 +1,28 @@
+class Solution {
+      func countPartitions(_ nums: [Int], _ k: Int) -> Int {
+        let mod = 1_000_000_007
+        var total = 0
+        var res = 1
+        var dp = [Int](repeating: 0, count: k)
+        dp[0] = 1
+
+        for a in nums {
+            for i in stride(from: k - 1 - a, through: 0, by: -1) {
+                dp[i + a] = (dp[i + a] + dp[i]) % mod
+            }
+
+            res = (res * 2) % mod
+            total += a
+        }
+
+        for i in 0..<k {
+            if total - i < k {
+                res -= dp[i]
+            } else {
+                res -= (dp[i] * 2) % mod
+            }
+        }
+
+        return (res % mod + mod) % mod
+    }
+}
