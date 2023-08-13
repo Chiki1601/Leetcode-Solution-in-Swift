a7b5f37dc0cd7193b3c7c17a43dfff20e0e5f4e0
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 14:17:30 +0530

    Create 2616.swift

---
+++ 2501-3000/2616.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func minimizeMax(_ nums: [Int], _ p: Int) -> Int {
+        let sortedNums = nums.sorted()
+        var left = 0
+        var right = sortedNums.last! - sortedNums.first!
+
+        while left < right {
+            let mid = (left + right) / 2
+            var k = 0
+            var i = 1
+
+            while i < sortedNums.count && k < p {
+                if sortedNums[i] - sortedNums[i - 1] <= mid {
+                    k += 1
+                    i += 2
+                } else {
+                    i += 1
+                }
+            }
+
+            if k >= p {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return left
+    }
+}
