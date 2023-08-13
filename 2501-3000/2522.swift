723c01ef629878a559af6ea5225a4c55bee25039
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 13:21:39 +0530

    Create 2522.swift

---
+++ 2501-3000/2522.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func minimumPartition(_ s: String, _ k: Int) -> Int {
+        var n: Int64 = 0
+        var res: Int = 1
+        for char in s {
+            let digit = Int64(String(char))!
+            n = n * 10 + digit
+            if n > Int64(k) {
+                res += 1
+                n = digit
+            }
+            if n > Int64(k) {
+                return -1
+            }
+        }
+        return res
+    }
+}
