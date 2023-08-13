c6dc6b2af3582d18b0841f2a2a44ce8d812b9b78
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 13:34:40 +0530

    Create 2540.swift

---
+++ 2501-3000/2540.swift
@@ -0,0 +1,18 @@
+class Solution {
+
+
+    func getCommon(_ nums1: [Int], _ nums2: [Int]) -> Int {
+        var i = 0
+        var j = 0
+        while i < nums1.count && j < nums2.count {
+            if nums1[i] == nums2[j] {
+                return nums1[i]
+            } else if nums1[i] < nums2[j] {
+                i += 1
+            } else {
+                j += 1
+            }
+        }
+        return -1
+    }
+}
