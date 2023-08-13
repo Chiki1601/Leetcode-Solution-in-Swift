8b534c3ac5b55f4bc777ae54cd71a7fe80d3244b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 14:08:32 +0530

    Create 2600.swift

---
+++ 2501-3000/2600.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func kItemsWithMaximumSum(_ numOnes: Int, _ numZeros: Int, _ numNegOnes: Int, _ k: Int) -> Int {
+        let ones = min(numOnes, k)
+        let zeros = min(numZeros, k - ones)
+        let negOnes = min(numNegOnes, k - ones - zeros)
+        return ones - negOnes + min(numOnes - ones, k - ones)
+            - min(numNegOnes - negOnes, k - ones - zeros - negOnes)
+    }
+}
