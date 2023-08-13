a288cd7e326394575ee22abbcf730ee4b98caa03
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-08-13 13:25:59 +0530

    Create 2529.swift

---
+++ 2501-3000/2529.swift
@@ -0,0 +1,18 @@
+class Solution {
+
+  
+    func maximumCount(_ nums: [Int]) -> Int {
+        var posCount = 0
+        var negCount = 0
+
+        for num in nums {
+            if num > 0 {
+                posCount += 1
+            } else if num < 0 {
+                negCount += 1
+            }
+        }
+
+        return max(posCount, negCount)
+    }
+}
