34ca1090931cba64cf8de6528f3e220f1fd6fedc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-08-01 08:59:07 +0530

    Create 118.swift

---
+++ 101-200/118.swift
@@ -0,0 +1,18 @@
+func generate(_ numRows: Int) -> [[Int]] {
+    var result: [[Int]] = []
+    for row in 0..<numRows {
+        let count = row + 1
+        
+        var array: [Int] = []
+        for column in 0..<count {
+            if column == 0 || column == count - 1 {
+                array.append(1)
+            } else {
+                let preArray = result[row - 1]
+                array.append(preArray[column - 1] + preArray[column])
+            }
+        }
+        result.append(array)
+    }
+    return result
+}
